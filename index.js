9e48cdb6796718e80699bb550a258be71a2ec27a
alihassanoo <57753201+alihassanoo@users.noreply.github.com>
2024-03-09 20:39:17 +0500

    updated index.js
    display all commands included

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
-
 const { program } = require('commander');
 const commandsDB = require('./git_commands.json'); // Your commands JSON file
 
 program
-  .name("buddy")
+  .name("shellbuddy")
   .description("CLI tool to provide shell commands for common questions")
   .version("1.0.0");
 
+// Command to display all available commands
+program.command('display all commands')
+  .description('Display all available commands')
+  .action(() => {
+    console.log('Available Commands:');
+    for (const query in commandsDB.buddy) {
+      console.log(`- ${query}`);
+    }
+  });
+
+// Command to get a specific shell command for a query
 program
   .arguments('<query...>')
   .description("Get the shell command for a query")
