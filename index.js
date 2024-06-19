cd805794d9c226e3c5baea35d6c372bc46bc5c13
alihassanoo <alihassanm381@gmail.com>
2024-06-19 19:11:40 +0500

    virutal env command added try 3

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function activateVirtualEnv(venvDir) {
   });
 
   ptyProcess.write(`source ${venvDir}/bin/activate\n`);
-  ptyProcess.write(`exec $SHELL\n`);  // Start a new shell with the activated environment
+  ptyProcess.write(`exec ${process.env.SHELL}\n`);
 }
 
 async function interactWithLlama3(prompt) {
